a94da4d2fee3252d1acda642a7e4c0121f3f3d85
Steven Yesandy Candelario Fernández <steven_candelario@ucne.edu.do>
2024-10-15 16:15:23 -0400

    Agrega models necesarios

---
+++ Albums.Data/Models/Albums.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Albums.Data.Models;
+
+public class Albums
+{
+    [Key]
+    public int AlbumId { get; set; }
+    [Required(ErrorMessage = "The album title is required")]
+    public string? AlbumTitle { get; set; }
+    [Required(ErrorMessage = "The artist name is required")]
+    public string? ArtistName { get; set; }
+    [Required(ErrorMessage = "The song number is required")]
+    public int? SongNumber { get; set; }
+    [Required(ErrorMessage = "The release year is required")]
+    public int? ReleaseYear { get; set; }
+    [Required(ErrorMessage = "The genre is required")]
+    [ForeignKey("GenreId")]
+    public int GenreId { get; set; }
+}
---
+++ Albums.Data/Models/Genres.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Albums.Data.Models;
+
+public class Genres
+{
+    [Key]
+    public int GenreId { get; set; }
+    [Required(ErrorMessage = "The genre name is requied")]
+    public string? GenreName { get; set; }
+}
