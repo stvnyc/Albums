84a7ec2307dafceb0139ca584901f362f78e4512
Steven Yesandy Candelario Fernández <steven_candelario@ucne.edu.do>
2024-10-15 16:44:44 -0400

    Crea migracion

---
+++ Albums.Api/Migrations/20241015204348_Inicial.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Albums.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "album",
+                columns: table => new
+                {
+                    AlbumId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    AlbumTitle = table.Column<string>(type: "TEXT", nullable: false),
+                    ArtistName = table.Column<string>(type: "TEXT", nullable: false),
+                    SongNumber = table.Column<int>(type: "INTEGER", nullable: false),
+                    ReleaseYear = table.Column<int>(type: "INTEGER", nullable: false),
+                    GenreId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_album", x => x.AlbumId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "genre",
+                columns: table => new
+                {
+                    GenreId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    GenreName = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_genre", x => x.GenreId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "album");
+
+            migrationBuilder.DropTable(
+                name: "genre");
+        }
+    }
+}
