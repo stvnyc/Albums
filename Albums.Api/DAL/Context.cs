3ca6cb55795c1ecac69fce36397a49557f4e43dd
Steven Yesandy Candelario Fernández <steven_candelario@ucne.edu.do>
2024-10-15 16:40:19 -0400

    Agrega contexto

---
+++ Albums.Api/DAL/Context.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Albums.Data.Models;
+
+namespace Albums.Api.DAL;
+
+public class Context : DbContext
+{
+    public DbSet<Genre> genre { get; set; }
+    public DbSet<Album> album { get; set; }
+    public Context(DbContextOptions<Context> options) : base(options) { }
+}
